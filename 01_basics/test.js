090cea7103f3aab87c267d68a6fc18c0eb48c8ed
Zahir Shaikh <152019891+Zahhiiir@users.noreply.github.com>
2024-07-13 05:47:15 +0000

    assingment 01

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,13 @@
 console.log("hi this is zahir!")
+const person = {
+    name : "zahir",
+    rollno : 700,
+    email : "zahir@google.com"
+}
+console.log(person);
+// var age = 19
+// console.log("Age =",age);
+// let zodiac = "capricon"
+// console.log("Zodiac =",zodiac)
+// const minor = false
+// console.log(minor);
