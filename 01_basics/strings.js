405706b7ba7dc0aaf0ae955a7cca04b10bafc331
Zahir Shaikh <152019891+Zahhiiir@users.noreply.github.com>
2024-10-11 17:34:00 +0000

    made new file cahnges

---
+++ 01_basics/strings.js
@@ -0,0 +1,7 @@
+const firstname = "zahir";
+const Lastname = "shaikh";
+
+// console.log(firstname.toUpperCase());
+// console.log(firstname.slice(0,3));
+// console.log(firstname.split(", "))
+ console.log(`helo ${firstname} ${Lastname}`)
